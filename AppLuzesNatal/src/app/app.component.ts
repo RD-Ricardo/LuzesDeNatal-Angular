843ac2f9c9293f56cc3bbdff1f08c623e0049931
RD <ricardodias169@gmail.com>
2021-04-30 19:11:23 -0300

    Finalizando App

---
+++ AppLuzesNatal/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { style } from '@angular/animations';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+
+
+}
